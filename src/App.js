b7160da0373dd77971b99f041aac47af5a18209e
Rosca Ana <arosca217@gmail.com>
2023-05-25 15:10:49 +0300

    Lab5.1

--- src/App.js
+++ src/App.js
@@ -1,234 +1,208 @@
 import React, { useState } from "react";
 import "./App.css";
 
-interface BasicFormInput {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  age: number;
+class BasicForm extends React.Component {
+    state = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        age: 0,
+        success: false,
+    };
+
+    handleInputChange = (event) => {
+        const { name, value } = event.target;
+        this.setState((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const { firstName, lastName, email, password, age } = this.state;
+        this.props.onSubmit({ firstName, lastName, email, password, age });
+        this.setState({ success: true });
+    };
+
+    render() {
+        const { firstName, lastName, email, password, age, success } = this.state;
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    First Name
+                    <input
+                        type="text"
+                        name="firstName"
+                        value={firstName}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Last Name
+                    <input
+                        type="text"
+                        name="lastName"
+                        value={lastName}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Email
+                    <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Password
+                    <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Age
+                    <input
+                        type="number"
+                        name="age"
+                        value={age}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+                {success && <p>Data loaded successfully!</p>}
+            </form>
+        );
+    }
 }
-
-interface BasicFormProps {
-  onSubmit: (data: BasicFormInput) => void;
-}
-
-interface BasicFormState extends BasicFormInput {
-  success: boolean;
-}
-
-class BasicForm extends React.Component<BasicFormProps, BasicFormState> {
-  state: BasicFormState = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    age: 0,
-    success: false,
-  };
-
-  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    this.setState((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const { firstName, lastName, email, password, age } = this.state;
-    this.props.onSubmit({ firstName, lastName, email, password, age });
-    this.setState({ success: true });
-  };
-
-  render() {
-    const { firstName, lastName, email, password, age, success } = this.state;
-    return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            First Name
-            <input
-                type="text"
-                name="firstName"
-                value={firstName}
-                onChange={this.handleInputChange}
-            />
-          </label>
-          <br />
-          <label>
-            Last Name
-            <input
-                type="text"
-                name="lastName"
-                value={lastName}
-                onChange={this.handleInputChange}
-            />
-          </label>
-          <br />
-          <label>
-            Email
-            <input
-                type="email"
-                name="email"
-                value={email}
-                onChange={this.handleInputChange}
-            />
-          </label>
-          <br />
-          <label>
-            Password
-            <input
-                type="password"
-                name="password"
-                value={password}
-                onChange={this.handleInputChange}
-            />
-          </label>
-          <br />
-          <label>
-            Age
-            <input
-                type="number"
-                name="age"
-                value={age}
-                onChange={this.handleInputChange}
-            />
-          </label>
-          <br />
-          <button type="submit">Submit</button>
-          {success && <p>Data loaded successfully!</p>}
-        </form>
-    );
-  }
-}
-
-interface ExtendedFormInput extends BasicFormInput {
-  address: string;
-  phone: string;
-}
-
-interface ExtendedFormProps extends BasicFormProps {}
-
-interface ExtendedFormState extends BasicFormState, ExtendedFormInput {}
 
 class ExtendedForm extends BasicForm {
-  state: ExtendedFormState = {
-    ...super.state,
-    address: "",
-    phone: "",
-  };
+    state = {
+        ...super.state,
+        address: "",
+        phone: "",
+    };
 
-  handleInputChange = (
-      event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const { name, value } = event.target;
-    this.setState((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
+    handleInputChange = (event) => {
+        const { name, value } = event.target;
+        this.setState((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
 
-  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const { firstName, lastName, email, password, age, address, phone } = this.state;
-    this.props.onSubmit({ firstName, lastName, email, password, age, address, phone });
-    this.setState({ success: true });
-  };
-  render() {
-    const { firstName, lastName, email, password, age, address, phone, success } = this.state;
-    return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            First Name
-            <input
-                type="text"
-                name="firstName"
-                value={firstName}
-                onChange={this.handleInputChange}
-            />
-          </label>
-          <br />
-          <label>
-            Last Name
-            <input
-                type="text"
-                name="lastName"
-                value={lastName}
-                onChange={this.handleInputChange}
-            />
-          </label>
-          <br />
-          <label>
-            Email
-            <input
-                type="email"
-                name="email"
-                value={email}
-                onChange={this.handleInputChange}
-            />
-          </label>
-          <br />
-          <label>
-            Password
-            <input
-                type="password"
-                name="password"
-                value={password}
-                onChange={this.handleInputChange}
-            />
-          </label>
-          <br />
-          <label>
-            Age
-            <input
-                type="number"
-                name="age"
-                value={age}
-                onChange={this.handleInputChange}
-            />
-          </label>
-          <br />
-          <label>
-            Address
-            <input
-                type="text"
-                name="address"
-                value={address}
-                onChange={this.handleInputChange}
-            />
-          </label>
-          <br />
-          <label>
-            Phone
-            <input
-                type="text"
-                name="phone"
-                value={phone}
-                onChange={this.handleInputChange}
-            />
-          </label>
-          <br />
-          <button type="submit">Submit</button>
-          {success && <p>Data loaded successfully!</p>}
-        </form>
-    );
-  }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const { firstName, lastName, email, password, age, address, phone } = this.state;
+        this.props.onSubmit({ firstName, lastName, email, password, age, address, phone });
+        this.setState({ success: true });
+    };
+
+    render() {
+        const { firstName, lastName, email, password, age, address, phone, success } = this.state;
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    First Name
+                    <input
+                        type="text"
+                        name="firstName"
+                        value={firstName}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Last Name
+                    <input
+                        type="text"
+                        name="lastName"
+                        value={lastName}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Email
+                    <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Password
+                    <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Age
+                    <input
+                        type="number"
+                        name="age"
+                        value={age}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Address
+                    <input
+                        type="text"
+                        name="address"
+                        value={address}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Phone
+                    <input
+                        type="text"
+                        name="phone"
+                        value={phone}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+                {success && <p>Data loaded successfully!</p>}
+            </form>
+        );
+    }
 }
 
 const App = () => {
-  const handleSubmit = (data: FormInput) => {
-    console.log(data);
-  };
+    const handleSubmit = (data) => {
+        console.log(data);
+    };
 
-  return (
-      <div className="App">
-        <p>Basic Interface </p>
-        <BasicForm onSubmit={handleSubmit} />
-        <p> Extended Interface </p>
-        <ExtendedForm onSubmit={handleSubmit} />
-      </div>
-  );
+    return (
+        <div className="App">
+            <p>Basic Interface</p>
+            <BasicForm onSubmit={handleSubmit} />
+            <p>Extended Interface</p>
+            <ExtendedForm onSubmit={handleSubmit} />
+        </div>
+    );
 };
 
 export default App;
