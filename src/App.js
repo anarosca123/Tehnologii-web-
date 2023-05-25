27139da6f2e98408609d6dbb7afa42bb72365451
Rosca Ana <arosca217@gmail.com>
2023-05-25 15:53:12 +0300

    Lab7

--- src/App.js
+++ src/App.js
@@ -1,88 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Menu from './Menu';
+import LoginPage from './GenInfoPage';
+import ContentPage from './ArhMemPage';
+import FormPage from './ApplPage';
+
+const menuListStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100vh', // added to make the div full-height
+    backgroundImage: "url('./src/fig-1-13-800x658.jpg')", // change URL to your image
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+};
+
+const contentStyle = {
+    marginTop: '50px',
+    marginBottom: '50px',
+    padding: '20px',
+    border: '2px solid #ccc',
+    borderRadius: '5px',
+    backgroundColor: 'white',
+};
+
+const titleStyle = {
+    fontSize: '24px',
+    fontWeight: 'bold',
+    marginBottom: '20px',
+};
+
+const buttonStyle = {
+    display: 'inline-block',
+    padding: '10px',
+    backgroundColor: '#f2f2f2',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    marginBottom: '20px',
+};
 
 function App() {
-    const [users, setUsers] = useState([]);
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [loggedIn, setLoggedIn] = useState(false);
-
-    useEffect(() => {
-        // Check if user data exists in localStorage
-        const storedUsers = JSON.parse(localStorage.getItem('users'));
-
-        if (storedUsers) {
-            setUsers(storedUsers);
-        } else {
-            // Create mock users
-            const mockUsers = [
-                { id: 1, username: 'user1', password: 'pass1' },
-                { id: 2, username: 'user2', password: 'pass2' },
-                { id: 3, username: 'user3', password: 'pass3' },
-            ];
-
-            // Store mock users in localStorage
-            localStorage.setItem('users', JSON.stringify(mockUsers));
-
-            // Set state with mock users
-            setUsers(mockUsers);
-        }
-    }, []);
-
-    const handleLogin = (event) => {
-        event.preventDefault();
-
-        // Find user with given username
-        const user = users.find((user) => user.username === username);
-
-        if (user && user.password === password) {
-            // Successful authentication
-            setLoggedIn(true);
-        } else {
-            // Authentication error
-            alert('(!) ERROR. Invalid LOGIN ...');
-        }
-    };
-
-    const handleLogout = () => {
-        setLoggedIn(false);
-    };
-
-    if (loggedIn) {
-        return (
-            <div style={{ textAlign: 'center', fontFamily: 'Arial, sans-serif', borderRadius: '10px' }}>
-                <h1 style={{ backgroundColor: '#f2f2f2', padding: '1rem', borderRadius: '10px 10px 0 0' }}>You are logged in!</h1>
-                <button style={{ marginTop: '1rem', backgroundColor: '#f2f2f2', padding: '0.5rem 1rem', borderRadius: '5px', cursor: 'pointer' }} onClick={handleLogout}>
-                    Log out
-                </button>
-            </div>
-        );
-    }
-
     return (
-        <form style={{ textAlign: 'center', fontFamily: 'Arial, sans-serif', borderRadius: '10px' }} onSubmit={handleLogin}>
-            <label style={{ display: 'block', marginTop: '1rem' }}>
-                Username:
-                <input
-                    type="text"
-                    style={{ marginLeft: '1rem', padding: '0.5rem', borderRadius: '5px', border: '1px solid #ccc' }}
-                    value={username}
-                    onChange={(event) => setUsername(event.target.value)}
-                />
-            </label>
-            <label style={{ display: 'block', marginTop: '1rem' }}>
-                Password:
-                <input
-                    type="password"
-                    style={{ marginLeft: '1rem', padding: '0.5rem', borderRadius: '5px', border: '1px solid #ccc' }}
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                />
-            </label>
-            <button style={{ marginTop: '1rem', backgroundColor: '#f2f2f2', padding: '0.5rem 1rem', borderRadius: '5px', cursor: 'pointer' }} type="submit">
-                Log in
-            </button>
-        </form>
+        <div style={menuListStyle}>
+            <div style={contentStyle}>
+                <div style={titleStyle}>AVR ATMega32 MICROCONTROLLER Information.</div>
+                <Link to="/" style={buttonStyle}>Return to Menu</Link>
+                <Routes>
+                    <Route path="/" element={<Menu />} />
+                    <Route path="/genInfo" element={<LoginPage />} />
+                    <Route path="/ArhMem" element={<ContentPage />} />
+                    <Route path="/Appl" element={<FormPage />} />
+                </Routes>
+            </div>
+        </div>
     );
 }
 
 export default App;
+
---
+++ src/ArhMemPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ArhMemPage() {
+    return (
+        <div>
+            <h1> ... Architecture and MCU MEMORY (KB) ... </h1>
+            <h3> >>> Architecture: AVR (8 Bit). </h3>
+            <h4> >>> MCU Memory : 32 KB of Flash / 2KB of Static RAM. </h4>
+        </div>
+    );
+}
+
+export default ArhMemPage;
